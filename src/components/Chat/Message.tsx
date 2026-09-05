12d95e8e8536a608fca6e82b16f2b9ad5d280653
Chiranshu <chiranshu@Chiranshus-MacBook-Pro.local>
2026-09-05 18:19:10 +0530

    feat: added markdown for assistant text UI

--- src/components/Chat/Message.tsx
+++ src/components/Chat/Message.tsx
@@ -1,3 +1,6 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 import { Message as MessageType } from "@/types/chat"
 import styles from "./message.module.scss"
 
@@ -11,7 +14,15 @@ const Message = ({ message }: MessageProps) => {
   return (
     <div className={`${styles['msg-container']} ${isUser ? styles['msg-user'] : styles['msg-assistant']}`}>
       <div className={styles['msg-wrapper']}>
-        {!isUser && !message.content ? "Thinking..." : message.content}
+        {!isUser && !message.content ? (
+          "Thinking..."
+        ) : isUser ? (
+          message.content
+        ) : (
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {message.content}
+          </ReactMarkdown>
+        )}
       </div>
     </div>
   )
